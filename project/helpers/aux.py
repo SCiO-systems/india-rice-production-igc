ae9a4ecaa9b589a5d45015e87d13088f4574e6f6
Yiorgos Chochlakis <yiorgoschochlakis@gmail.com>
2019-07-08 16:12:13 +0300

    Improve readability, crop GEOTIFF and export to WGS84
    In particular:
    
    - Add comments to make repo more readable
    - Dispose of useless info from geotiff using bounding box
    - Attempt at exporting geotiff in pre-agreed coordinates system
    - Emphasize distinction between array indices and cartesian coordinates
    - Correct parameters of `SetGeoTranform`

--- project/helpers/aux.py
+++ project/helpers/aux.py
@@ -2,6 +2,7 @@ import numpy as np
 from matplotlib.path import Path
 
 def Coords2Indices(point, cellsize=0.16666666667, xllcorner=-180, yllcorner=-60):
+    # point in (>, Λ) => (x,y) reversed for matrix conventions (V, >)
     x = int(np.floor((point[0] - xllcorner) / cellsize))
     y = int(np.floor((point[1] - yllcorner) / cellsize))
     return (y, x)
@@ -9,10 +10,16 @@ def Coords2Indices(point, cellsize=0.16666666667, xllcorner=-180, yllcorner=-60)
 def Coords2IndicesAr(points, cellsize=0.16666666667, xllcorner=-180, yllcorner=-60):
     coords = []
     for point in points:
+        # NOTE: duplicates because of upsampling (replace with (ordered) set?)
         coords.append(Coords2Indices(point, cellsize, xllcorner, yllcorner))
     return coords
 
 def upsampleGeoJSON(coords_ar, rate):
+    '''
+    Upsamples coordinates parsed from a GEOJSON file
+    by addind coordinates on the line between consecutive
+    coordinates
+    '''
     if rate == 1: raise ValueError('Upsample rate cannot be 1')
     if rate != int(rate): raise ValueError('Upsample rate must be integer')
 
@@ -44,16 +51,59 @@ def ascRead(filename):
             elif line.startswith('NODATA'):
                 nodataval = int(line.split()[1])
             else:
+                # .asc format provides data from top left point in rows
                 data.append([int(d) for d in line.split()])
             line = f.readline()
 
     return nrows, ncols, xllcorner, yllcorner, cellsize, nodataval, np.array(data)
 
 def fillPolygon4Raster(perimeter, rows=900, cols=2160):
+    '''
+    Given the indices of the perimeter, returns the filled polygon
+    (inclusive) 
+    '''
     x, y = np.meshgrid(np.arange(rows), np.arange(cols))
     x, y = x.flatten(), y.flatten()
     geo_points = np.vstack((x,y)).T
 
     p = Path(perimeter)
     grid = p.contains_points(geo_points)
+    # np.flip(axis=0) to get [0,0] as top left point of raster
     return np.flip(grid.reshape(cols, rows).T, 0)
+
+def boundingBoxAr(coords, nodataval=-9999):
+    def boundOneDimension(a):
+        n, m = a.shape
+        foundmin = False
+        foundmax = True
+        for i in range(n):
+            for j in range(m):
+                if not foundmin and a[i,j] != nodataval:
+                    foundmin = True
+                    tl = i
+                if a[i,j] != nodataval:
+                    foundmax = False
+            if foundmin and foundmax:
+                lr = i
+                break
+            else:
+                foundmax = True
+        return tl, lr
+
+    # 1 connected component
+    coords = np.array(coords)
+    in_itl, in_ilr = boundOneDimension(coords)
+    in_jtl, in_jlr = boundOneDimension(coords.T)
+    return in_itl, in_jtl, in_ilr, in_jlr
+
+def boundingBoxPer(perimeter, rows):
+    in_itl = min([i for i,_ in perimeter])
+    in_jtl = min([j for _,j in perimeter])
+    in_ilr = max([i for i,_ in perimeter])
+    in_jlr = max([j for _,j in perimeter])
+    # because of np.flip in fillPolygon4Raster, xs must be reversed (rows - _)
+    return rows - in_ilr - 1, in_jtl, rows - in_itl - 1, in_jlr
+
+def padding(bb, rows, cols, pad=3):
+    return max(bb[0] - pad, 0), max(bb[1] - pad, 0), \
+        min(bb[2] + pad, rows-1), min(bb[3] + pad, cols-1)
--- project/project.py
+++ project/project.py
@@ -14,27 +14,39 @@ with open(geojson_fn, 'r') as f:
 
 perimeter = osgeo.ogr.CreateGeometryFromJson(geojson)
 
-rows, cols, xllcorner, yllcorner, cellsize, nodataval, data = aux.ascRead(bio_fn)
-
 indian_coords = osgeo.ogr.Geometry.GetPoints(
     osgeo.ogr.Geometry.GetGeometryRef(perimeter, 0)
 )
-indian_coords = aux.upsampleGeoJSON(indian_coords, 15)
+# upsample with heuristic rate 15 to fill gaps in array indices
+indian_coords = aux.upsampleGeoJSON(indian_coords, rate=15)
+
+# data array holds at [0,0] the top left point of the actual data
+rows, cols, xllcorner, yllcorner, cellsize, nodataval, data = aux.ascRead(bio_fn)
 
 perimeter = aux.Coords2IndicesAr(
     indian_coords, xllcorner=xllcorner, yllcorner=yllcorner, cellsize=cellsize
 )
 
+# get "rough" polygon of india and set values outside of it to NODATA_value
 india = aux.fillPolygon4Raster(perimeter, rows, cols)
-
 data[np.invert(india)] = nodataval
 
+# get bounding box of india for geotiff purposes
+in_itl, in_jtl, in_ilr, in_jlr = aux.padding(
+    aux.boundingBoxPer(perimeter, rows), rows, cols, pad=3
+)
+
+
 drv = gdal.GetDriverByName('GTiff')
-ds = drv.Create('./results/test.tiff', cols, rows, 1, gdal.GDT_Byte)
+ds = drv.Create('./results/test.tiff', in_jlr - in_jtl, in_ilr - in_itl, 1, gdal.GDT_Byte)
 ds.SetGeoTransform((
-    xllcorner, cellsize, 0, yllcorner + rows * cellsize, 0, cellsize
+    xllcorner + in_itl * cellsize, cellsize, 0, yllcorner + (rows - in_itl) * cellsize, 0, -cellsize
 ))
-ds.SetProjection(osr.SpatialReference().ExportToWkt())
-ds.GetRasterBand(1).WriteArray(data)
+
+dst_srs = osr.SpatialReference()
+# NOTE: attempt at setting crs to wgs84 (epsg 3857), to be verified
+dst_srs.ImportFromEPSG(3857)
+ds.SetProjection(dst_srs.ExportToWkt())
+ds.GetRasterBand(1).WriteArray(data[in_itl:in_ilr, in_jtl:in_jlr])
 ds.GetRasterBand(1).SetNoDataValue(nodataval)
 ds.GetRasterBand(1).FlushCache()
