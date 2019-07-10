79032e9bb28c72a12facce860a6d6349580858f0
Yiorgos Chochlakis <yiorgoschochlakis@gmail.com>
2019-07-10 22:24:44 +0300

    Add rest of relevant dataset and create multiband geotiff.

---
+++ data/CMIP5/README.md
@@ -0,0 +1,15 @@
+# Dataset structure and data explanation
+
+* Structure
+  * bcm_csm_1_1_m : model used
+  * rcp : Representation Concentration Pathways - possible future emmision trajectories
+  * 2080s : 2070 - 2099 measurements aggregation
+  * 10min : based on reported data per 10 minutes
+  * rXiXpX : realization, initialization, physics - initial state of ensemble calculations
+
+* Data
+  * cons : Consecutive dry months
+  * prec : Precipitation
+  * tmax : Monthly maximum temperature
+  * tmean : Monthly mean temperature
+  * tmin : Monthly minimum temperature
--- project/project.py
+++ project/project.py
@@ -1,9 +1,10 @@
 import osgeo, gdal, osr
 import helpers.aux as aux
+import helpers.bands as bands
 import numpy as np
 
 geojson_fn = './data/IND.geo.json'
-bio_fn = './data/tmax_1.asc'
+
 
 geojson = ''
 with open(geojson_fn, 'r') as f:
@@ -20,33 +21,52 @@ indian_coords = osgeo.ogr.Geometry.GetPoints(
 # upsample with heuristic rate 15 to fill gaps in array indices
 indian_coords = aux.upsampleGeoJSON(indian_coords, rate=15)
 
-# data array holds at [0,0] the top left point of the actual data
-rows, cols, xllcorner, yllcorner, cellsize, nodataval, data = aux.ascRead(bio_fn)
+# dummy read to initialize other structures
+rows, cols, xllcorner, yllcorner, cellsize, nodataval, data = aux.ascRead(
+    bands.BANDS[0]['path'].replace(bands.TEMPLATE, str(bands.BANDS[0]['range'][0]))
+)
 
-perimeter = aux.Coords2IndicesAr(
+# get perimeter and cropping params
+perimeterIndices = aux.Coords2IndicesAr(
     indian_coords, xllcorner=xllcorner, yllcorner=yllcorner, cellsize=cellsize
 )
-
-# get "rough" polygon of india and set values outside of it to NODATA_value
-india = aux.fillPolygon4Raster(perimeter, rows, cols)
-data[np.invert(india)] = nodataval
-
-# get bounding box of india for geotiff purposes
 in_itl, in_jtl, in_ilr, in_jlr = aux.padding(
-    aux.boundingBoxPer(perimeter, rows), rows, cols, pad=3
+    aux.boundingBoxPer(perimeterIndices, rows), rows, cols, pad=3
 )
 
-
+# create empty tiff
 drv = gdal.GetDriverByName('GTiff')
-ds = drv.Create('./results/test.tiff', in_jlr - in_jtl, in_ilr - in_itl, 1, gdal.GDT_Byte)
+ds = drv.Create('./results/india_cmip5_multiband.tiff', in_jlr - in_jtl, in_ilr - in_itl, bands.getNumberOfBands(), gdal.GDT_Byte)
 ds.SetGeoTransform((
     xllcorner + in_itl * cellsize, cellsize, 0, yllcorner + (rows - in_itl) * cellsize, 0, -cellsize
 ))
 
+bandcnt = 1
+for band in bands.BANDS:
+    for i in band['range']:
+        print(bandcnt)
+        # data array holds at [0,0] the top left point of the actual data
+        rows, cols, xllcorner, yllcorner, cellsize, nodataval, data = aux.ascRead(
+            # if band['path'] does not contain TEMPLATE, nothing is replaced
+            # and only one iteration is made
+            band['path'].replace(bands.TEMPLATE, str(i))
+        )
+
+        perimeterIndices = aux.Coords2IndicesAr(
+            indian_coords, xllcorner=xllcorner, yllcorner=yllcorner, cellsize=cellsize
+        )
+
+        # get "rough" polygon of india and set values outside of it to NODATA_value
+        india = aux.fillPolygon4Raster(perimeterIndices, rows, cols)
+        data[np.invert(india)] = nodataval
+
+        ds.GetRasterBand(bandcnt).WriteArray(data[in_itl:in_ilr, in_jtl:in_jlr])
+        ds.GetRasterBand(bandcnt).SetNoDataValue(nodataval)
+
+        bandcnt += 1
+
 dst_srs = osr.SpatialReference()
-# NOTE: attempt at setting crs to wgs84 (epsg 3857), to be verified
+# setting crs to wgs84 (epsg 3857)
 dst_srs.ImportFromEPSG(3857)
 ds.SetProjection(dst_srs.ExportToWkt())
-ds.GetRasterBand(1).WriteArray(data[in_itl:in_ilr, in_jtl:in_jlr])
-ds.GetRasterBand(1).SetNoDataValue(nodataval)
-ds.GetRasterBand(1).FlushCache()
+ds.GetRasterBand(bandcnt-1).FlushCache()
